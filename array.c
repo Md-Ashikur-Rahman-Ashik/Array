fb80fffdf1d134d731c5300f77fe7a6858c944fd
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-16 08:34:01 +0600

    Accessed elements of an array

--- array.c
+++ array.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-    int arr[10];
+    // int arr[10];
+    int ar[5];
+    ar[2] = 10;
+    // printf("%d", ar[2]);
+
+    ar[3] = 20;
+    printf("%d", ar[3]);
+
+    ar[1] = 50;
 
     return 0;
 }
