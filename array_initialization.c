82a75a1eef1d2d64b5072065cbfad298913ef1d7
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-16 15:18:46 +0600

    Explored array initialization

---
+++ array_initialization.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // int arr[5] = {10, 20, 30, 40, 50};
+    int arr[5] = {0};
+
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
